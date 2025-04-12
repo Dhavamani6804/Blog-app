cb06ece3675ca4fe64447258b64f2064e02fc1ad
Dhavamani6804 <sdhavamani22@gmail.com>
2025-04-13 00:27:29 +0530

    created signup page ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,83 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function signup() {
-  return <div>signup</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl flex-col md:flex-row md:items-center gap-5 mx-auto">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className=" text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-blue-400 via-blue-600 to-blue-700 rounded-lg text-white">
+              Dhava's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is my blog where you can catch up all my new ideas
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                required={true}
+                className="mb-3"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <TextInput
+                type="text"
+                placeholder="user@mail.com"
+                id="email"
+                required={true}
+                className="mb-3"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <TextInput
+                type="text"
+                placeholder="********"
+                id="password"
+                required={true}
+                className="mb-3"
+              />
+            </div>
+            <Button gradientDuoTone="purpleToBlue" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Hava an account?</span>
+            <Link to="/signin" className="text-blue-500 ml-2">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
