2288e64eb12540886c5a9c47b17c33bc359bff4c
Dhavamani6804 <sdhavamani22@gmail.com>
2025-04-12 11:20:41 +0530

    header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,11 @@ import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-
+import Header from "./components/Header";
 export default function AppRoutes() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import {
+  Button,
+  Navbar,
+  TextInput,
+  NavbarLink,
+  NavbarToggle,
+  NavbarCollapse,
+} from "flowbite-react";
+
+export default function Header() {
+  const location = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Dhava's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="grey" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="">
+        <Button className="w-12 h-10 hidden sm:inline" color="grey" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/signin">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink active={location === "/"} as={Link} to="/">
+          Home
+        </NavbarLink>
+        <NavbarLink active={location === "/about"} as={Link} to="/about">
+          About
+        </NavbarLink>
+        <NavbarLink active={location === "/projects"} as={Link} to="/projects">
+          Projects
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
