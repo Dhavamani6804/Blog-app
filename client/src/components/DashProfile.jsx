f83cb9f93a1fc671d3e0d1d7a30fe055b2ce68c9
Dhavamani6804 <sdhavamani22@gmail.com>
2025-04-20 17:11:03 +0530

    added user image upload functions

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import defaultAvatar from "../assets/user.png";
 import { Button, TextInput } from "flowbite-react";
 
 export default function DashProfile() {
   const currentUser = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    console.log("Uploading image...");
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shodow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shodow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current?.click()}
+        >
           <img
-            src={currentUser.profilePicture || defaultAvatar}
+            src={imageFileUrl || currentUser.profilePicture || defaultAvatar}
             alt="User"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightfray]"
+            className="rounded-full w-full h-full object-cover border-8 border-gray-300
+"
           />
         </div>
         <TextInput
