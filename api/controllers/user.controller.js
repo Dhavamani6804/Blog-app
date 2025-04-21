69c927670d8339373bb6563ba721de557a5132c5
Dhavamani6804 <sdhavamani22@gmail.com>
2025-04-21 23:33:31 +0530

    added function for updation of user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,21 +1,61 @@
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "API is working" });
 };
 
-export const updateUser = async(req, res,next) => {
-if(req.user.id!=req.params.userId){
-  return next(errorHandler(403,"You are not allowed to  update this user!"))
-}
-if(req.body.password){
-if(req.body.password.length<6){
-  return next(errorHandler(400,"Password should be at least 6 characters long!"))
-}
-req.body.password=await bcrypt.hash(req.body.password,10);
-}
-if (req.body.username){
-  if(req.body.username.length<3){
-    return next(errorHandler(400,"Username must be between 7 and 20 characters!"))
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user!"));
   }
-}
+
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "Password should be at least 6 characters long!")
+      );
+    }
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  if (req.body.username) {
+    if (req.body.username.length < 3 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 3 and 20 characters!")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username should not contain spaces!"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username should be in lowercase!"));
+    }
+    if (!/^[a-zA-Z0-9]+$/.test(req.body.username)) {
+      return next(
+        errorHandler(400, "Username should not contain special characters!")
+      );
+    }
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
