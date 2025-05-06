3555af70e4bab797fa8c84cef20705a4d940af85
Dhavamani6804 <sdhavamani22@gmail.com>
2025-05-06 21:26:15 +0530

    ui corrections

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -47,9 +47,12 @@ export default function SignIn() {
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
-          <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahand's
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 rounded-lg text-white">
+              Dhava's
             </span>
             Blog
           </Link>
@@ -80,11 +83,7 @@ export default function SignIn() {
                 onChange={handleChange}
               />
             </div>
-            <Button
-              gradientDuoTone="purpleToPink"
-              type="submit"
-              disabled={loading}
-            >
+            <Button className="rounded-tl-xl rounded-bl-none bg-gradient-to-r from-blue-800 via-blue-600 to-blue-500 rounded-lg rounded-br-xl w-full hover:from-blue-500 hover:via-blue-600 hover:to-blue-80 transform hover:scale-95 transition duration-300">
               {loading ? (
                 <>
                   <Spinner size="sm" />
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -42,9 +42,12 @@ export default function SignUp() {
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
-          <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahand's
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 rounded-lg text-white">
+              Dhava's
             </span>
             Blog
           </Link>
@@ -84,11 +87,7 @@ export default function SignUp() {
                 onChange={handleChange}
               />
             </div>
-            <Button
-              gradientDuoTone="purpleToPink"
-              type="submit"
-              disabled={loading}
-            >
+            <Button className="rounded-tl-xl rounded-bl-none bg-gradient-to-r from-blue-800 via-blue-600 to-blue-500 rounded-lg rounded-br-xl w-full hover:from-blue-500 hover:via-blue-600 hover:to-blue-80 transform hover:scale-95 transition duration-300">
               {loading ? (
                 <>
                   <Spinner size="sm" />
